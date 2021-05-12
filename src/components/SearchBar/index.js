2d51a4cd43c972330d9c23b008a39cd9fcb45922
wahoostik <anthonypaccaud@wanadoo.fr>
2021-05-12 14:57:14 +0200

    composant searchbar statique

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Form, Segment } from 'semantic-ui-react';
 
 import './styles.scss';
 
 const SearchBar = (() => (
-  <div>SearchBar</div>
+  <Segment>
+    <Form>
+      <Form.Input
+        icon="search"
+        iconPosition="left"
+        placeholder="Chercher un repo..."
+      />
+    </Form>
+  </Segment>
 ));
 
 export default SearchBar;
